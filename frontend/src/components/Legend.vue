635b509e0ba7e14e129ac1aabce7158924656e63
Patrick Schwering <patrick.schwering@gmail.com>
2025-04-11 21:49:34 +0200

    add darkmode colors

--- frontend/src/components/Legend.vue
+++ frontend/src/components/Legend.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="mt-4 mb-4 bg-white">
+  <div class="mt-4 section mb-4 bg-white">
     <div class="p-4">
       <h2 class="text-xl font-bold mb-2">Regeln für die Eintragung</h2>
       <p class="mb-2">
         Freitag bis 12 Uhr kann ein Salat für die folgende Woche eingetragen werden.<br />
         Einträge die nach dieser Zeit getätigt werden, werden automatisch als "reserviert"
-        <span class="inline-block text-center w-8 h-8 py-1 event-rejected"><img src="@/assets/salat.svg" class="inline-block" alt="salat icon"></span> markiert.
+        <span class="inline-block text-center w-8 h-8 py-1 event-reserved"><img src="@/assets/salat.svg" class="inline-block" alt="salat icon"></span> markiert.
       </p>
       <p>
         Einträge mit einem Papierkorb können noch von Dir selber gelöscht werden.
@@ -14,7 +14,7 @@
   </div>
 
   <div class="mb-4">
-    <div class="mb-4 flex items-center space-x-4 bg-white">
+    <div class="mb-4 section flex items-center space-x-4 bg-white">
       <div class="inline-block p-4 event-approved">
         <img src="@/assets/salat.svg" alt="salat icon">
       </div>
@@ -23,7 +23,7 @@
       </p>
     </div>
 
-    <div class="mb-4 flex items-center space-x-4 bg-white">
+    <div class="mb-4 section flex items-center space-x-4 bg-white">
       <div class="inline-block p-4 event-reserved">
         <img src="@/assets/salat.svg" alt="salat icon">
       </div>
@@ -32,7 +32,7 @@
       </p>
     </div>
 
-    <div class="mb-4 flex items-center space-x-4 bg-white">
+    <div class="mb-4 section flex items-center space-x-4 bg-white">
       <div class="inline-block p-4 event-rejected">
         <img src="@/assets/salat.svg" alt="salat icon">
       </div>
@@ -42,5 +42,3 @@
     </div>
   </div>
 </template>
-<script setup lang="ts">
-</script>
